72bdefc3f28237e3659268ee9ab437cd15616e87
Leonardo Raele <leonardoraele@gmail.com>
2026-08-09 11:38:11 -0300

    feat: Add extension methods for Task to handle completion, fault, and cancellation scenarios

--- src/extensions/TaskExtensionMethods.cs
+++ src/extensions/TaskExtensionMethods.cs
@@ -1,5 +1,37 @@
+using System;
+using System.Threading.Tasks;
+
 namespace Raele.GodotUtils.Extensions;
 
 public static class TaskExtensionMethods
 {
+	extension(Task self)
+	{
+		public Task OnCompletedSuccessfully(Action action)
+		{
+			return self.ContinueWith(task =>
+			{
+				if (!task.IsCompletedSuccessfully)
+					action();
+			});
+		}
+
+		public Task OnFaulted(Action<Exception> action)
+		{
+			return self.ContinueWith(task =>
+			{
+				if (task.IsFaulted && task.Exception != null)
+					action(task.Exception);
+			});
+		}
+
+		public Task OnCanceled(Action action)
+		{
+			return self.ContinueWith(task =>
+			{
+				if (task.IsCanceled)
+					action();
+			});
+		}
+	}
 }
